d4a44a6e48789df1c45fe32b323dd2f71d9f74ad
BBQuercus <bastian@eichenbergers.ch>
2020-05-21 18:53:54 +0200

    Minor spelling corrections.

--- src/views/Biology.vue
+++ src/views/Biology.vue
@@ -4,17 +4,17 @@
       type="Biology"
       title="Current"
       image="./images/current.jpeg"
-      text="In the lab of <a href='https://www.fmi.ch/research-groups/groupleader.html?group=132' target='_blank'>Jeffrey Chao</a> at the Friedrich Miescher Institute for Biomedical Research in Basel (Switzerland), I'm investigating stress granules. Using various single-molecule imaging techniques ranging from SunTag to TREAT, I'm interested to determine the effect that stress granule proteins (namely G3BP1) exert on mRNAs with regards to translation rates, stability, and localization. As these techniques have their limitations I try to improve on them and make them even more useful. Current plans are to establish a new SunTag imaging method relying on the structural support by Spaghetti Monster.
+      text="In the lab of <a href='https://www.fmi.ch/research-groups/groupleader.html?group=132' target='_blank'>Jeffrey Chao</a> at the Friedrich Miescher Institute for Biomedical Research in Basel (Switzerland), I'm investigating stress granules. Using various single-molecule imaging techniques ranging from SunTag to TREAT, I'm interested to determine the effect that stress granule proteins (namely G3BP1) exert on mRNAs with regards to translation rate, stability, and localization. As these techniques have their limitations, I try to improve on them and make them even more useful. Current plans are to establish a new SunTag imaging method relying on the structural support by Spaghetti Monster.
       
-      Besides the wet lab, various computational tasks also require my attention. In addition to typical thresholding based image-analysis, I'm developing a deep-learning-based platform called Fluffy (check the coding section). Fluffy's focus is two-fold. First, it tries to adapt state-of-the-art methods in the fields of image segmentation and diffraction-limited spot detection. Secondly, through a simple web interface, it tries to make deep learning more accessible than ever before."
+      Besides the wet lab, various computational tasks also require my attention. In addition to typical thresholding based image-analysis, I'm developing a deep-learning-based platform called Fluffy (check the coding section). Fluffy's focus is two-fold. First, it tries to adapt state-of-the-art methods in the fields of image segmentation and diffraction-limited spot detection. Second, through a simple web interface, it tries to make deep learning more accessible than ever before."
     />
     <Post
       type="Biology"
       title="Previous"
       image="./images/past.jpeg"
-      text="During my Bachelor studies at the Albert Ludwigs University of Freiburg im Brsg. (Germany) I had the privilege of studying small nematodes called Caenorhabditis elegans in the <a href='https://celegans.de/' target='_blank'>Baumeister lab</a>. My goal was to investigate an endoribonuclease called endu-2. Thanks to many sacrificed worms, I was able to show various protein-RNA interactions and potential involvement in DNA methylation.
+      text="During my Bachelor studies at the Albert Ludwigs University of Freiburg im Brsg. (Germany), I had the privilege of studying small nematodes called <i>Caenorhabditis elegans</i> in the <a href='https://celegans.de/' target='_blank'>Baumeister lab</a>. My goal was to investigate an endoribonuclease called <i>endu-2</i>. Thanks to many sacrificed worms, I was able to show various protein-RNA interactions and potential involvement in DNA methylation.
       
-      An extended stay as research associate benefited me by solidifying staple lab techniques ranging from making home-made competent cells and DNA polymerase as well as a plethora of worm-specific applications."
+      An extended stay as a research associate benefited me by solidifying staple lab techniques ranging from making home-made competent cells and DNA polymerase as well as a plethora of worm-specific applications."
     />
   </div>
 </template>
--- src/views/Coding.vue
+++ src/views/Coding.vue
@@ -6,7 +6,7 @@
       image="./images/portfolio.jpeg"
       github="https://github.com/BBQuercus/portfolio"
       website="http://bastianeichenberger.ch"
-      text="After getting comfortable with the basics front-end web stack in HTML, CSS, and JavaScript, it was time to tackle a larger challenge. Choosing a framework wasn't easy but Vue JS seemed like a good fit. After understanding what routes are and how to bind data, I quickly started appreciating Vue's simplicity and the ability to reuse code snippets. I added a few animations here and there to make the website feel more alive but still have a long way to go."
+      text="After getting comfortable with the basics front-end web stack in HTML, CSS, and JavaScript, it was time to tackle a larger challenge. Looking through all potential frameworks, I ended up settling on Vue. After understanding what routes are and how to bind data, I quickly started appreciating Vue's simplicity and the ability to reuse code components. I added a few animations here and there to make the website feel more alive, but still have a long way to go."
       hashes="#Vue JS #Custom CSS #UI/UX Design"
     />
     <Post
@@ -15,7 +15,7 @@
       image="./images/apnea.jpeg"
       github="https://github.com/BBQuercus/appnea"
       website="http://apnea.netlify.com"
-      text="I enjoy spending time underwater. Usually though, I carry a supply of fresh air when going SCUBA diving. Nonetheless, it's always a good idea to carry more air in your own lungs. Trying to increase my lung capacity I wasn't able to find a free, easy to use a timer to practice apnea. To get more familiar with vanilla JS the project seemed ideal. Check it out if you want to join me explore the magical underwater world."
+      text="I enjoy spending time underwater. Usually though, I carry a supply of fresh air when going SCUBA diving. Nonetheless, it's always a good idea to carry more air in your own lungs. Trying to increase my lung capacity I wasn't able to find a free, easy-to-use timer to practice apnea. To get more familiar with vanilla JS the project seemed ideal. Check it out if you want to join me explore the magical underwater world."
       hashes="#Custom CSS #Vanilla JS #UI Design"
     />
     <Post
@@ -24,7 +24,7 @@
       image="./images/fluffy.jpeg"
       github="http://github.com/BBQuercus/fluffy"
       website="http://hub.docker.com/r/bbquercus/fluffy"
-      text="Besides setting up a simple <a href='http://minisoberwil.ch' target='_blank'>Wordpress site</a> for my local church this was my first real project. After developing deep-learning models from biomedical image analysis I had no idea how to distribute them. After long hours of experimentation, the combination of a basic Docker container and a Flask application worked out. To get things going quickly I used Bootstrap for styling. Although the code isn't following the standards I set today, it definitely worked well enough and simplified many's image analysis."
+      text="Besides setting up a simple <a href='http://minisoberwil.ch' target='_blank'>Wordpress site</a> for my local church, this was my first real project. After developing deep-learning models from biomedical image analysis, I had no idea how to distribute them. After long hours of experimentation, the combination of a basic Docker container and a Flask backend worked out. To get things going quickly I used Bootstrap for styling. Although the code isn't following the standards I set today, it definitely worked well enough and simplified many's image analysis."
       hashes="#Flask #Bootstrap4 #jQuery #Docker"
     />
   </div>
