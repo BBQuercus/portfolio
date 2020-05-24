91ba93b395b3cd9e42f0520beef5d6755336a498
BBQuercus <bastian@eichenbergers.ch>
2020-05-24 15:46:05 +0200

    Full height site. Added my name to home.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="home">
     <h1>
-      <router-link to="about">Hey</router-link>&nbsp; there, I’m a
-      <router-link to="coding">coding</router-link>&nbsp; molecular
+      <router-link to="about">Hey</router-link>&nbsp; there, I’m Bastian. <br/>
+      A <router-link to="coding">coding</router-link>&nbsp; molecular
       <router-link to="biology">biologist</router-link>&nbsp; and learning enthusiast.
     </h1>
   </div>
